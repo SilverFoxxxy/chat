621ce5ff12ddceceefa8b5291c9c70358f847727
SilverFoxxxy <ivan-belkov2@yandex.ru>
2023-05-03 08:51:57 +0300

    Update auto_login.js

--- auto_login.js
+++ auto_login.js
@@ -19,18 +19,19 @@ async function check_token() {
         cur_url = window.location.href;
         if (cur_url.includes('belkovanya')) {
             redirect('./', "login.html");
-            redirect('./index.html', "login.html");
+            // redirect('./index.html', "login.html");
         } else {
             redirect('./index.html', "login.html");
         }
     } else {
         cur_url = window.location.href;
         console.log(cur_url.substr(cur_url.lastIndexOf('/') + 1));
-        if (cur_url.includes('belkovanya')) {
-            redirect('./login.html', "/");
-        } else {
-            redirect("./login.html", 'index.html');
-        }
+        redirect('./login.html', "");
+        // if (cur_url.includes('belkovanya')) {
+            
+        // } else {
+        //     redirect("./login.html", 'index.html');
+        // }
         // if (cur_url.substr(cur_url.lastIndexOf('/') + 1) == 'index.html') { 
         //     window.location.href = "./login.html";
         // }
